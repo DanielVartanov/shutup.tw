2319476a5d929b0135407bde3bcd7b3ccbe50920
Daniel Vartanov <dan@vartanov.net>
2014-06-29 17:29:07 +0600

    Use genuine current user in MutesController

--- app/controllers/mutes_controller.rb
+++ app/controllers/mutes_controller.rb
@@ -4,7 +4,6 @@ class MutesController < ApplicationController
   end
 
   def create
-    current_user = User.last
     current_user.mute params[:twitter_name]
     redirect_to :new_mute, notice: 'Muted!'
   end
